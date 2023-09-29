e45d28905b2ecc4d5f626395139420c6069c456b
user <katya.rotar02@gmail.com>
2023-09-29 18:07:28 +0300

    Task_1 and Task_2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -9,15 +9,59 @@
         Console.Write("Висота: ");
         double height = double.Parse(Console.ReadLine());
         Box box = new Box(length, width, height);
-        Console.WriteLine($"Площа поверхні = {box.Surface_Area()}\n" +
-            $"Площа бічної поверхні = {box.Lateral_Surface_Area()}\n" +
-            $"Об’єм = {box.Volume()}");
+        if (length > 0 && width > 0 && height > 0)
+        {
+            Console.WriteLine($"Площа поверхні = {box.Surface_Area()}\n" +
+                $"Площа бічної поверхні = {box.Lateral_Surface_Area()}\n" +
+                $"Об’єм = {box.Volume()}");
+        }
     }
 }
 class Box {
-    private double Length { get; set; }
-    private double Width { get; set; }
-    private double Height { get; set; }
+    private double length;
+    public double Length {
+        get { return length; }
+        private set
+        {
+            if (value > 0)
+            {
+                length = value;
+            }
+            else {
+                Console.WriteLine("Length cannot be zero or negative.");
+            }
+        } 
+    }
+    private double width;
+    public double Width {
+        get { return width; }
+        private set
+        {
+            if (value > 0)
+            {
+                width = value;
+            }
+            else
+            {
+                Console.WriteLine("Width cannot be zero or negative.");
+            }
+        } 
+    }
+    private double height;
+    public double Height {
+        get { return height; }
+        private set
+        {
+            if (value > 0)
+            {
+                height = value;
+            }
+            else
+            {
+                Console.WriteLine("Width cannot be zero or negative.");
+            }
+        }
+    }
 
     public Box(double length, double width, double height)
     {
